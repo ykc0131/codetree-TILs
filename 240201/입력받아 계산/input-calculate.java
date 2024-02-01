9c7b86f88431e7bd50ea808cefc18410380e4be4
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-02-01 17:19:24 +0900

    Title: 입력받아 계산, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240201/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt(); // 정수를 입력받기 위함
+        System.out.println(a+2);
+    }
+}
