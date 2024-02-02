d0ddf3e8a8117e46f985e1354da570af61c85e1b
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-02-03 02:06:02 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 282ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240203/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class Main {
+    static class Node{
+        int node;
+        int cost;
+
+        Node(int node, int cost){
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+    static final int MAX_N = 100001;
+    static Scanner sc = new Scanner(System.in);
+    static int n,m;
+    static boolean[] visit;
+    static int[] dist;
+    static ArrayList<Node>[] graph;
+
+    public static void init(){
+        n = sc.nextInt();
+        m = sc.nextInt();
+        visit = new boolean[n+1];
+        graph = new ArrayList[n+1];
+
+        for(int i=0; i<n+1; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<m; i++){
+            int u,v,cost;
+
+            u = sc.nextInt();
+            v = sc.nextInt();
+            cost = sc.nextInt();
+            graph[u].add(new Node(v,cost));
+        }
+    }
+
+    public static void dij(){
+        dist = new int[n+1];
+        Arrays.fill(dist,MAX_N);
+        PriorityQueue<Node> pq = new PriorityQueue<>(
+            (a,b)->a.cost-b.cost //크면 바꾼다 = 오름차순
+        );
+
+        pq.add(new Node(1,0));
+        dist[1]=0;
+        
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            
+            if(visit[cur.node])
+                continue;
+            visit[cur.node] = true;
+
+            for(Node node : graph[cur.node]){
+                int next = node.node;
+                int nextCost = node.cost + cur.cost;
+
+                if(!visit[next] && dist[next] > nextCost){
+                    dist[next] = nextCost;
+                    pq.add(new Node(next, nextCost));
+                }
+            }
+        }
+    }
+
+    public static void solve(){
+        dij();
+        for(int i=2; i<=n; i++){
+            System.out.println(dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        init();
+        solve();
+    }
+}
