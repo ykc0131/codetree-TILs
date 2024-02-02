72e2521c7bcf8403457554742cfeca2e90becce7
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-02-02 18:00:22 +0900

    Title: 정수 명령 처리, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240202/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Stack<Integer> s = new Stack<>();
+        for(int i=0; i<n; i++){
+            String st = sc.next();
+            
+            if(st.equals("push")){
+                int num = sc.nextInt();
+                s.push(num);
+            }
+            else if(st.equals("size")){
+                System.out.println(s.size());
+            }
+            else if(st.equals("empty")){
+                if(s.empty())
+                    System.out.println(1);
+                else
+                    System.out.println(0);
+            }
+            else if(st.equals("pop")){
+                System.out.println(s.peek()); s.pop();
+            }
+        }
+    }
+}
