b358c119d98d25a419cf20a99d01b7143a422af6
yookibee <61011209+ykc0131@users.noreply.github.com>
2024-03-15 16:45:42 +0900

    Title: 최대 숫자 만들기, Time: 18ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240315/최대 숫자 만들기/make-biggest-num.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int n;
+vector<int> vec;
+void init(){
+    cin >> n;
+    vec.resize(n,0);
+    for(int i=0; i<n; i++){
+        cin >> vec[i];
+    }
+}
+
+bool customeCmp(int a, int b){
+    int cmp1 = a*pow(10,to_string(b).size()) + b;
+    int cmp2 = b*pow(10,to_string(a).size()) + a;
+
+    return cmp1 > cmp2;
+}
+
+void solve(){
+    sort(vec.begin(), vec.end(), customeCmp);
+
+    string result = "";
+    for(auto v : vec){
+        result += to_string(v);
+    }
+
+    cout << result << "\n";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    init();
+    solve();
+    return 0;
+}
